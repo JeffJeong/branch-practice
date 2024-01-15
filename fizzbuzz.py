179578f03c3b2be42804e67dd6648ab67fadaeb4
JeffJeong <jbw140@gmail.com>
2024-01-15 14:13:33 +0900

    feat: create fizzbuzz
    implement fizz function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,8 +2,6 @@ def fizzbuzz():
     for i in range(1,15+1):
         if(i%3 == 0):
             print('fizz')
-        elif(i%5 == 0):
-            print('buzz')
         else:
             print(i)
 
