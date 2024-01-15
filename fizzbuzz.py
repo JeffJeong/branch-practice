025adf57805a775364175dbc016a4a3f4905992c
JeffJeong <jbw140@gmail.com>
2024-01-15 14:11:32 +0900

    feat: implementation of buzz function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,7 @@ def fizzbuzz():
     for i in range(1,15+1):
         if(i%3 == 0):
             print('fizz')
-        elif(i%5 ==0):
+        elif(i%5 == 0):
             print('buzz')
         else:
             print(i)
